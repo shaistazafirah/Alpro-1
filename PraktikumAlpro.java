2dd5edd642b9711fea74e16e42a4863223ca29b1
Shaista Zafirah <shaistazafirah2867@gmail.com>
2025-09-17 20:09:58 +0700

    Praktikum Alpro 1

---
+++ PraktikumAlpro.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikumalpro1;
+
+/**
+ *
+ * @author LAB_KOMPUTASI_17
+ */
+public class PraktikumAlpro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to Alpro 1! and I'm ready to study");
+        System.out.println("This is mmy first time");
+        System.out.println("Wish me luck!");
+        
+        String nama = "Rain";
+        System.out.println(nama);
+        
+        char huruf = 'S';
+        System.out.println(huruf);
+        
+    }
+    
+}
